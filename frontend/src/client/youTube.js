0437780bab67cf47aac3a02d6d761c6674b2ad79
Chris Vita <chris.vita@gmail.com>
2017-11-21 10:34:31 -0800

    Add YouTube key publicly
    No need obfuscate. Restricted use set in Google Developer Console.

--- frontend/src/client/youTube.js
+++ frontend/src/client/youTube.js
@@ -1,6 +1,6 @@
 const fetchVideos = (keywords, maxResults) => (
   new Promise((resolve, reject) => {
-    const key = process.env.youTubeKey;
+    const key = 'AIzaSyAxXnGEhkkZmEh7zfugJpAsJ7kpSU4GbDc'; // Restricted usage
     const request = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${keywords}&regionCode=US&type=video&videoCategoryId=10&videoDuration=short&videoEmbedable=true&key=${key}`;
     fetch(request, { method: 'GET' })
       .then(resp => resp.json())
