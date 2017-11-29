db18b8d06978d633a3eb0cdf787685e29e2f8756
Chris Vita <chris.vita@gmail.com>
2017-11-29 15:30:37 -0800

    Replace iframe with `react-youtube`
    Enables player control via js.

--- frontend/src/components/Player/YouTubePlayer.js
+++ frontend/src/components/Player/YouTubePlayer.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
+import YouTube from 'react-youtube';
 
-
-const Player = props => {
-  const src = `https://www.youtube.com/embed/${props.id}?enablejsapi=1`;
-  const videoWrapperStyle = {
-    position: 'relative',
-    padding: '25px 0 56.25% 0' /* 16:9 */
-  };
-  const iframeStyle = {
-    width: '100%',
-    height: '100%',
-    position: 'absolute',
-  };
-  return (
-    <div style={{ minWidth: '250px' }}>
-      <div style={videoWrapperStyle}>
-        <iframe
-          src={src}
-          title={props.title}
-          style={iframeStyle}
-          type='text/html'
-          frameBorder='0'
-          allowFullScreen
-        />
-      </div>
-    </div>
-  );
-};
 
 class YouTubePlayer extends Component {
   render() {
-    const { videos } = this.props;
-    if (videos.length > 0) {
-      const id = videos[0].id;
-      const title = videos[0].title;
-      return (
-        <Player id={id} title={title} />
-      );
-    }
-    return null;
+    const { id } = this.props;
+
+    const videoWrapperStyle = {
+      position: 'relative',
+      padding: '25px 0 56.25% 0' /* 16:9 */
+    };
+    const iframeStyle = {
+      width: '100%',
+      height: '100%',
+      position: 'absolute',
+    };
+
+    const options = {
+      height: '100%',
+      width: '100%',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        enablejsapi: 1
+      }
+    };
+
+    return (
+      <div style={{ minWidth: '250px' }}>
+        <div style={videoWrapperStyle}>
+          <div style={iframeStyle}>
+            <YouTube
+              videoId={id}
+              opts={options}
+              onReady={this._onReady}
+              onEnd={this.props.handleEnd}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    // event.target.pauseVideo();
   }
 }
+
 
 export default YouTubePlayer;
