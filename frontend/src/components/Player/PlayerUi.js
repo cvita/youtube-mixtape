3449d7401627160d5ba24f5b158a341438ebd5ba
Chris Vita <chris.vita@gmail.com>
2017-12-06 13:54:33 -0800

    Refactor `<Player />` and subcomponents

--- frontend/src/components/Player/PlayerUi.js
+++ frontend/src/components/Player/PlayerUi.js
@@ -1,56 +1,48 @@
-import React, {Component} from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 import './PlayerUi.css';
 
 
-class PlayerUi extends Component {
-  constructor(props) {
-    super(props);
-    this.togglePlaying = this.togglePlaying.bind(this);
-    this.state = { playing: true };
-  }
-  togglePlaying() {
-    const playOrPause = this.state.playing ? 'pause' : 'play'; // make opposite action
-    this.props.handleControl(playOrPause);
-    this.setState({ playing: !this.state.playing });
-  }
-  render() {
-    const { artist, video, playerState, handleControl } = this.props;
-    const playPauseLabel = this.state.playing ? 'pause' : 'play';
-    const title = video.title
-      .replace(artist.name, '')
-      .replace('-', '')
-      .trim();
+const PlayerUi = props => {
+  const { playerCollapsed, playerState, onClick } = props;
+  const { artist, video } = props.selectedArtist;
 
-    return (
-      <Container>
-        <Row>
-          <Col lg='2' md='3' sm='4' xs='12'>
-            {!playerState.collapse && (
-              <img className='playerUiThumbnail' src={video.thumbnails.default.url} alt={artist.name} />)}
-          </Col>
+  const playPauseAction = playerState === 1 ? 'pause' : 'play';
+  const playPauseIcon = playerState === 1 || playerState === 2 ? playPauseAction : 'cog fa-spin';
 
-          <Col lg='4' md='3' sm='8' xs='12'>
-            <div className='playerUiTitle'>
-              <h1>{artist.name}</h1>
-              <h2>{title}</h2>
-            </div>
-          </Col>
+  const title = video.title
+    .replace(artist.name, '')
+    .replace('-', '')
+    .trim();
 
-          <Col md='6' sm='12' xs='12'>
-            <div className='playerUiButtons'>
-              <button className='fa fa-step-backward' onClick={() => this.props.handleNewSong('previous')} />
-              <button className='fa fa-fast-backward' onClick={() => handleControl('fastRewind')} />
-              <button className={`fa fa-${playPauseLabel}`} onClick={this.togglePlaying} id='playPause' />
-              <button className='fa fa-fast-forward' onClick={() => handleControl('fastForward')} />
-              <button className='fa fa-step-forward' onClick={() => this.props.handleNewSong('next')} />
-            </div>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Row>
+        <Col lg='2' md='3' sm='4' xs='12'>
+          {playerCollapsed && (
+            <img className='playerUiThumbnail' src={video.thumbnails.default.url} alt={artist.name} />)}
+        </Col>
+
+        <Col lg='4' md='3' sm='8' xs='12'>
+          <div className='playerUiTitle'>
+            <h1>{artist.name}</h1>
+            <h2>{title}</h2>
+          </div>
+        </Col>
+
+        <Col md='6' sm='12' xs='12'>
+          <div className='playerUiButtons'>
+            <button className='fa fa-step-backward' onClick={() => onClick('previous')} />
+            <button className='fa fa-fast-backward' onClick={() => onClick('fastRewind')} />
+            <button className={`fa fa-${playPauseIcon}`} onClick={() => onClick(playPauseAction)} id='playPause' />
+            <button className='fa fa-fast-forward' onClick={() => onClick('fastForward')} />
+            <button className='fa fa-step-forward' onClick={() => onClick('next')} />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 
 export default PlayerUi;
--- frontend/src/components/Player/YouTubePlayer.js
+++ frontend/src/components/Player/YouTubePlayer.js
@@ -2,20 +2,60 @@ import React, { Component } from 'react';
 import YouTube from 'react-youtube';
 
 
+const youTubeContainerOuter = {
+  maxWidth: '75vh',
+  minWidth: '250px',
+  margin: '0 auto 2em auto'
+};
+const youTubeContainerInner = {
+  position: 'relative',
+  padding: '25px 0 56.25% 0' /* 16:9 */
+};
+const iframeStyle = {
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+};
+
 class YouTubePlayer extends Component {
   constructor(props) {
     super(props);
-    this.state = { player: null };
+    this.handleError = this.handleError.bind(this);
     this.handleOnReady = this.handleOnReady.bind(this);
+    this.handleStateChange = this.handleStateChange.bind(this);
     this.handleControl = this.handleControl.bind(this);
+    this.state = { player: null };
+  }
+  handleError(event) {
+    this.props.updatePlayer({
+      playerState: -1, // https://developers.google.com/youtube/iframe_api_reference
+      duration: 0,
+      elapsed: 0,
+      error: event.data
+    });
   }
   handleOnReady(event) {
     if (!this.state.player) {
-      this.setState({ player: event });
+      this.setState({ player: event.target }); // Access to YouTube API
+    }
+    this.handleStateChange(); // Update state prior to first video playback
+  }
+  handleStateChange() {
+    const { player } = this.state;
+    this.props.updatePlayer({
+      playerState: player.getPlayerState(),
+      duration: player.getDuration(),
+      elapsed: player.getCurrentTime(),
+      error: null
+    });
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.actionRequest !== nextProps.actionRequest) {
+      this.handleControl(nextProps.actionRequest.action);
     }
   }
   handleControl(control) {
-    const player = this.state.player.target;
+    const { player } = this.state;
     const currentTime = player.getCurrentTime();
     switch (control) {
       case 'play':
@@ -31,44 +71,26 @@ class YouTubePlayer extends Component {
     }
   }
   render() {
-    const { id, videoControl } = this.props;
-    const videoWrapperStyle = {
-      position: 'relative',
-      padding: '25px 0 56.25% 0' /* 16:9 */
-    };
-    const iframeStyle = {
-      width: '100%',
-      height: '100%',
-      position: 'absolute',
-    };
-    const options = {
-      height: '100%',
-      width: '100%',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-        enablejsapi: 1
-      }
-    };
+    const { selectedArtist, onEnd, onError } = this.props;
 
-    if (videoControl) {
-      this.handleControl(videoControl);
-    }
     return (
-      <div style={{ maxWidth: '75vh', minWidth: '250px', margin: '0 auto 2em auto' }}>
-        <div style={videoWrapperStyle}>
+      <div style={youTubeContainerOuter}>
+        <div style={youTubeContainerInner}>
           <div style={iframeStyle}>
             <YouTube
-              videoId={id}
-              opts={options}
+              videoId={selectedArtist.video.id}
               onReady={this.handleOnReady}
-              onEnd={this.props.handleEnd}
-              onStateChange={(event) => (
-                this.props.handleStateChange({
-                  playerState: event.target.getPlayerState(),
-                  duration: event.target.getDuration(),
-                  elapsed: event.target.getCurrentTime()
-                })
-              )}
+              onStateChange={this.handleStateChange}
+              onEnd={onEnd}
+              onError={event => {
+                this.handleError(event);
+                onError();
+              }}
+              opts={{
+                height: '100%',
+                width: '100%',
+                playerVars: { autoplay: 1, enablejsapi: 1 }
+              }}
             />
           </div>
         </div>
