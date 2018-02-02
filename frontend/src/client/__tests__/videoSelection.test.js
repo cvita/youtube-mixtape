02e74d26f6ff2386778c9952d3f3bd3ebef91144
Chris Vita <chris.vita@gmail.com>
2018-02-02 15:58:07 -0800

    Refactor for readability
    No need to resolve `artist` object.

--- frontend/src/client/__tests__/videoSelection.test.js
+++ frontend/src/client/__tests__/videoSelection.test.js
@@ -1,22 +1,21 @@
 import videoSelection from '../videoSelection';
-import youTubeSearchResults from './stubData/youTubeSearchResults.json'
+import videos from './stubData/youTubeSearchResults.json'
+import artist from './stubData/artist.json';
 import played from './stubData/played.json';
 
 
 describe('videoSelection:', () => {
   it('should return `artist` and `video` objects', () => {
-    return videoSelection.selectVideo(youTubeSearchResults, played)
+    return videoSelection.selectVideo(artist, videos, played)
       .then(res => {
-        expect(res.artist).toBeInstanceOf(Object);
-        expect(res.video).toBeInstanceOf(Object);
+        expect(res).toBeInstanceOf(Object);
       });
   });
 
   it('should handle an empty array as `played` argument', () => {
-    return videoSelection.selectVideo(youTubeSearchResults, [])
+    return videoSelection.selectVideo(artist, videos, [])
       .then(res => {
-        expect(res.artist).toBeInstanceOf(Object);
-        expect(res.video).toBeInstanceOf(Object);
+        expect(res).toBeInstanceOf(Object);
       });
   });
 });
--- frontend/src/client/videoSelection.js
+++ frontend/src/client/videoSelection.js
@@ -1,15 +1,13 @@
 // Todo: Add timestamp to selected video
 // Todo: Make sure video id is not in blacklist (stored in DB). Compare ID's
-const selectVideo = (youTubeSearchResult, played) => (
+const selectVideo = (artist, videos, played) => (
   new Promise((resolve, reject) => {
-    const { artist, videos } = youTubeSearchResult;
     const alreadyPlayed = played.filter(video => video.artist === artist.name).map(video => video.id);
     const unplayed = videos.filter(video => !alreadyPlayed.includes(video.id));
     const selectedVideo = unplayed.length > 0 ?
       unplayed[generateRandomIndex(unplayed)] :
-      videos[generateRandomIndex(videos)]; // Todo: Make new call to YouTube (w/ offset) for more videos
-
-    resolve({ artist, video: selectedVideo });
+      videos[generateRandomIndex(videos)];
+    resolve(selectedVideo);
   })
 );
 
